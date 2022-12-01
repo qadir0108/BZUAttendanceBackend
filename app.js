ef1744bcf2e657894af68b7475dd700c5a0f2c00
Kamran Qadir <qadir0108@hotmail.com>
2022-12-01 19:30:16 +0500

    added endpoints

--- app.js
+++ app.js
@@ -16,17 +16,18 @@ app.post('/students/login', (req, res) => {
   console.log('request recived');
   console.log(req.body);
   
-  var rollnumber = req.body['rollnumber'];
-  var password = req.body['password'];
+  var rollnumber = req.body['RollNumber'];
+  var password = req.body['Password'];
   var result = {};
   if(rollnumber == 'LDBTT-1923-01' && password == 'pass123') {
     result = {
-      success: true,
+      Success: true,
+      Name: "Rana Akram",
     };
   } else {
     result = {
-      success: false,
-      errorMessage: "Wrong username or password"
+      Success: false,
+      ErrorMessage: "Wrong RollNumber or Password"
     };
   }
   
@@ -35,37 +36,79 @@ app.post('/students/login', (req, res) => {
 
 })
 
-app.get('/timetable', (req, res) => {
+app.post('/timetable', (req, res) => {
   console.log('request recived');
 
   var result = 
   [
     {
       "Date": "20221125",
-      "Slot": 1,
+      "TimeSlot": 1,
       "SubjectCode": "IT205",
       "Subject": "Data Structure",
       "Teacher": "Kamran Qadir"
     },
     {
       "Date": "20221125",
-      "Slot": 2,
+      "TimeSlot": 2,
       "SubjectCode": "IT307",
       "Subject": "Visual Programming",
       "Teacher": "Kamran Qadir"
     },
     {
       "Date": "20221125",
-      "Slot": 3,      
+      "TimeSlot": 3,      
       "SubjectCode": "IT403",
       "Subject": "Mobile App Development",
       "Teacher": "Kamran Qadir"
-    },
-  ] 
-  ;
+    }
+  ];
 
   res.json(result);
   console.log('response sent');
+})
+
+app.post('/attendance/history', (req, res) => {
+  console.log('request recived');
+
+  var result = 
+  [
+    {
+      "Date": "20221125",
+      "TimeSlot": 1,
+      "SubjectCode": "IT205",
+      "Subject": "Data Structure",
+      "Teacher": "Kamran Qadir",
+      "Status": "PRESENT",
+    },
+    {
+      "Date": "20221125",
+      "TimeSlot": 2,
+      "SubjectCode": "IT307",
+      "Subject": "Visual Programming",
+      "Teacher": "Kamran Qadir",
+      "Status": "PRESENT",
+    },
+    {
+      "Date": "20221125",
+      "TimeSlot": 3,
+      "SubjectCode": "IT403",
+      "Subject": "Mobile App Development",
+      "Teacher": "Kamran Qadir",
+      "Status": "ABSENT",
+    },
+  ];
+
+  res.json(result);
+  console.log('response sent');
+})
+
+app.post('/attendance/mark', (req, res) => {
+  console.log('request recived');
+  console.log(req.body);
+  res.json(req.body);
+  console.log('response sent');
+
 })
 
 app.listen(port, () => {
